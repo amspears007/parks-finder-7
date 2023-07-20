abd5f3ce023502eab460b4b49c1e400954942bbf
Amy Spears <amspears007@gmail.com>
2023-07-20 16:32:55 -0600

    Add more test for feature page and create faraday conn and response in parks controller

--- app/controllers/parks_controller.rb
+++ app/controllers/parks_controller.rb
@@ -1,5 +1,14 @@
 class ParksController < ApplicationController
   def index
+    state = params[:state]
+    
+    conn = Faraday.new(url: "https://developer.nps.gov") do |faraday|
+    faraday.headers["X-Api-Key"] = ENV["X-Api-Key"]
+  end
+  
+  response = conn.get("/api/v1/parks?parkCode=&stateCode=#{state}&limit=10")
+  json = JSON.parse(response.body, symbolize_names: true)
+  require 'pry'; binding.pry
 
   end
 end
--- spec/features/find_parks_by_state_spec.rb
+++ spec/features/find_parks_by_state_spec.rb
@@ -7,5 +7,25 @@ RSpec.describe "State Park Search Page" do
     select "Tennessee", from: :state
     click_on "Find Parks"
     expect(current_path).to eq(parks_path) 
+    expect(page).to have_content("Park Total: 15")
+    expect(page).to have_content("Name: Andrew Johnson National Historic Site")
+    expect(page).to have_content("Operating Hours")
+    expect(page).to have_content("Description")
+
+    expect(page).to have_content("Direction Info:")
+
+
+
   end
 end
+# As a user,
+# When I select "Tennessee" from the form,
+# (Note: use the existing form)
+# And click on "Find Parks",
+# I see the total of parks found,
+# And for each park I see:
+# - full name of the park
+# - description
+# - direction info
+# - standard hours for operation
+```
